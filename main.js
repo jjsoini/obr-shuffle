293114e51901f93a80cb56df872a5e6cf9355c86
Jussi Soini <jjsoini@gmail.com>
2023-11-03 20:23:33 +0200

    more code

--- main.js
+++ main.js
@@ -2,12 +2,6 @@ import "./style.css";
 import OBR from "@owlbear-rodeo/sdk";
 import { setupContextMenu } from "./contextMenu";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <h1>Initiative Tracker</h1>
-  </div>
-`;
-
 OBR.onReady(() => {
   setupContextMenu();
 });
